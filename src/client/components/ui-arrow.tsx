a1a413add2b44ecd1cf7ff3894d8f043f016166d
Igor Shevnin <interphx@gmail.com>
2017-12-20 13:46:37 +0500

    WIP

--- src/client/components/ui-arrow.tsx
+++ src/client/components/ui-arrow.tsx
@@ -1,7 +1,12 @@
+import * as React from 'react';
+
 import { UIArrow } from 'client/view-model/ui-arrow';
+import { Vec2 } from 'client/util/vec2';
 
 
-
-export function UIArrowView({screenPosStart, screenPosEnd}) {
-
+export function UIArrowView({screenPosStart, screenPosEnd}: {screenPosStart: Vec2, screenPosEnd: Vec2}) {
+    return <line x1={screenPosStart.x}
+                 y1={screenPosStart.y}
+                 x2={screenPosEnd.x}
+                 y2={screenPosEnd.y} />
 }
